395cc84ef12fd808615ccf0458968689142902fa
iza17hor <iza17hor@gmail.com>
2026-03-11 19:20:12 +0100

    updated accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,17 +1,14 @@
 export default function decorate(block) {
 
   [...block.children].forEach((section) => {
-    const titleDiv = section.children[0]; 
-    const contentDiv = section.children[1]; 
+    const title = section.children[0]; 
+    const content = section.children[1]; 
 
 
-    titleDiv.style.cursor = 'pointer';
-    contentDiv.style.display = 'none';
-
-
-    titleDiv.addEventListener('click', () => {
-      contentDiv.style.display = contentDiv.style.display === 'block' ? 'none' : 'block';
-      section.classList.toggle('active');
+    title.addEventListener('click', () => {
+      const isVisible = content.style.display === 'block';
+      content.style.display = isVisible ? 'none' : 'block';
+      section.classList.toggle('active'); 
     });
   });
 }
