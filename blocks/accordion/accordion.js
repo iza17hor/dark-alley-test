baf20a62da977385995d3fa7990ab91080a704ec
iza17hor <iza17hor@gmail.com>
2026-04-23 16:10:41 +0200

    update tabs

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,35 +1,13 @@
-export default function decorate(block) {
-  const rows = [...block.children];
+function decorateAccordion(container) {
+  container.querySelectorAll('[data-item]').forEach((item) => {
+    const title = item.querySelector('[data-title]');
+    const content = item.querySelector('[data-content]');
 
-  rows.forEach((row, index) => {
-    const [title, content] = row.children;
-
-  
-    if (index !== 0) {
-      content.style.display = 'none';
-    } else {
-      row.classList.add('active');
-      content.style.display = 'block';
-    }
-
-    title.style.cursor = 'pointer';
+    content.style.display = 'none';
 
     title.addEventListener('click', () => {
-
-      const isOpen = row.classList.contains('active');
-
-   
-      rows.forEach((r) => {
-        r.classList.remove('active');
-        r.children[1].style.display = 'none';
-      });
-
-     
-      if (!isOpen) {
-        row.classList.add('active');
-        content.style.display = 'block';
-      }
-
+      content.style.display =
+        content.style.display === 'none' ? 'block' : 'none';
     });
   });
 }
