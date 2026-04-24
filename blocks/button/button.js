9d8127e7df870210c6b10d2377a00dac924adaba
iza17hor <iza17hor@gmail.com>
2026-04-24 10:33:13 +0200

    add button block

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,23 +1,7 @@
 export default function decorate(block) {
-  
-  
-  const rows = [...block.children];
-  const contentRow = rows[1]; 
+  const link = block.querySelector('a');
 
-  if (contentRow) {
-    const link = contentRow.querySelector('a');
-    
-    if (link) {
-   
-      link.classList.add('button-link');
-
-  
-      const textContent = contentRow.textContent.toLowerCase();
-      if (textContent.includes('primary')) link.classList.add('primary');
-      if (textContent.includes('secondary')) link.classList.add('secondary');
-
-
-      block.replaceChildren(link);
-    }
+  if (link) {
+    link.classList.add('button');
   }
 }
